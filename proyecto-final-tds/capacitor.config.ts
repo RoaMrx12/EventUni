c5ed755983e8dce530fac3771a736d3d0d7c5a31
jearinalvarado1 <20211477@itla.edu.do>
2024-08-02 22:46:24 -0400

    Proyecto

---
+++ proyecto-final-tds/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'proyecto.final.tds',
+  appName: 'proyecto-final-tds',
+  webDir: 'www'
+};
+
+export default config;
