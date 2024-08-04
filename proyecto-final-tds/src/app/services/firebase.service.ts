f776f92b1b2105033022c48757f14ffb920ac5c5
Diovennyff <20220467@itla.edu.do>
2024-08-03 23:15:40 -0400

    Añadido nuevo contenido en el directorio proyecto-final-tds

---
+++ proyecto-final-tds/src/app/services/firebase.service.ts
@@ -0,0 +1,70 @@
+import { inject, Injectable } from '@angular/core';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  sendPasswordResetEmail,
+} from 'firebase/auth';
+import { User } from '../models/user.model';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+
+import { getFirestore, setDoc, doc, getDoc } from '@angular/fire/firestore';
+import { UtilsService } from './utils.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirebaseService {
+  auth = inject(AngularFireAuth);
+  firestore = inject(AngularFirestore);
+  utilsSvc = inject(UtilsService);
+
+  //============== AUNTETICACION ==============//
+
+  getauth() {
+    return getAuth();
+  }
+
+  //========= Acceder =========//
+  singIn(user: User) {
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  //========= Crear usuario =========//
+  singUp(user: User) {
+    return createUserWithEmailAndPassword(getAuth(), user.email, user.password);
+  }
+
+  //========= Actualizar usuario =========//
+  updateUser(displayName: string) {
+    return updateProfile(getAuth().currentUser, { displayName });
+  }
+
+  //========= Enviar email para restablecer contraseña ===========//
+  sendRecoveryEmail(email: string) {
+    return sendPasswordResetEmail(getAuth(), email);
+  }
+
+  //===== Cerrar Seccion =====//
+  signOut() {
+    getAuth().signOut();
+    localStorage.removeItem('user');
+    this.utilsSvc.routerLink('/auth');
+  }
+
+  // ==================Base de datos==================//
+
+  //======= Setear un documento =======//
+  setDocument(path: string, data: any) {
+    return setDoc(doc(getFirestore(), path), data);
+  }
+
+  //======= Obtener un documento =======//
+  async getDocument(path: string) {
+    return (await getDoc(doc(getFirestore(), path))).data();
+  }
+}
---
+++ proyecto-final-tds/src/app/services/utils.service.ts
@@ -0,0 +1,42 @@
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  LoadingController,
+  ToastController,
+  ToastOptions,
+} from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilsService {
+  loadingCtrl = inject(LoadingController);
+  toastCtrl = inject(ToastController);
+  router = inject(Router);
+
+  //=====loadin=====//
+  loading() {
+    return this.loadingCtrl.create({ spinner: 'crescent' });
+  }
+
+  //======toas======//
+  async presentToast(opts?: ToastOptions) {
+    const toast = await this.toastCtrl.create(opts);
+    toast.present();
+  }
+
+  //======Enruta a cualquier pagina disponible======//
+  routerLink(url: string) {
+    return this.router.navigateByUrl(url);
+  }
+
+  //======Guarda un elemento en localstorage======//
+  saveInLocalStorage(key: string, value: any) {
+    return localStorage.setItem(key, JSON.stringify(value));
+  }
+
+  //======Obtiene un elemento desde localstorage======//
+  getFromLocalStorage(key: string) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+}
